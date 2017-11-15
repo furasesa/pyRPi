0517a62832906b5f6baba9d1775072c9d3a0d858
furasesa <angga.furasesa@gmail.com>
2017-11-15 09:19:09 +0700

    updated

--- server/tcptest_server.py
+++ server/tcptest_server.py
@@ -1,16 +1,71 @@
-import socket
+def do_some_stuffs_with_input(input_string):  
+    """
+    This is where all the processing happens.
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 2303))
-# become a server socket
-serversocket.listen(5)
+    Let's just read the string backwards
+    """
 
-while True:
-    # accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-    # now do something with the clientsocket
-    # in this case, we'll pretend this is a threaded server
-    ct = client_thread(clientsocket)
-    ct.run()
+    print("Processing that nasty input!")
+    return input_string[::-1]
+
+def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
+
+    # the input is in bytes, so decode it
+    input_from_client_bytes = conn.recv(MAX_BUFFER_SIZE)
+
+    # MAX_BUFFER_SIZE is how big the message can be
+    # this is test if it's sufficiently big
+    import sys
+    siz = sys.getsizeof(input_from_client_bytes)
+    if  siz >= MAX_BUFFER_SIZE:
+        print("The length of input is probably too long: {}".format(siz))
+
+    # decode input and strip the end of line
+    input_from_client = input_from_client_bytes.decode("utf8").rstrip()
+
+    res = do_some_stuffs_with_input(input_from_client)
+    print("Result of processing {} is: {}".format(input_from_client, res))
+
+    vysl = res.encode("utf8")  # encode the result string
+    conn.sendall(vysl)  # send it to client
+    conn.close()  # close connection
+    print('Connection ' + ip + ':' + port + " ended")
+
+def start_server():
+
+    import socket
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this is for easy starting/killing the app
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('Socket created')
+
+    try:
+        soc.bind(("", 2303))
+        print('Socket bind complete')
+    except socket.error as msg:
+        import sys
+        print('Bind failed. Error : ' + str(sys.exc_info()))
+        sys.exit()
+
+    #Start listening on socket
+    soc.listen(10)
+    print('Socket now listening')
+
+    # for handling task in separate jobs we need threading
+    from threading import Thread
+
+    # this will make an infinite loop needed for 
+    # not reseting server for every client
+    while True:
+        conn, addr = soc.accept()
+        ip, port = str(addr[0]), str(addr[1])
+        print('Accepting connection from ' + ip + ':' + port)
+        try:
+            Thread(target=client_thread, args=(conn, ip, port)).start()
+        except:
+            print("Terible error!")
+            import traceback
+            traceback.print_exc()
+    soc.close()
+
+start_server()
