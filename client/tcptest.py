5d1e9ffc71883773cd32fea5655afe2e3bd43329
furasesa <angga.furasesa@gmail.com>
2017-11-15 16:16:12 +0700

    u

--- client/tcptest.py
+++ client/tcptest.py
@@ -1,10 +1,12 @@
 import socket
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-soc.connect(("192.168.43.40", 2303))
+soc.connect(("localhost", 2303))
 
 import random  
 data = zip([random.randint(1,1000) for i in range(1000)],  
  [random.randint(1,1000) for i in range(1000)])
+
+data = zip(['test data from client'], ['asdasdwdda'])
 
 for x, y in data:  
     # send x and y separated by tab 
---
+++ server/rpi.py
@@ -0,0 +1,53 @@
+import json
+import lib.hwid as hwid
+import RPi.GPIO as GPIO
+from lib.dict import Dictionary
+
+
+try:
+    with open('config.json', 'r') as f:
+        data = json.load(f)
+        d = Dictionary(data)
+        global uuid
+        global mode
+        global ch_in
+        global ch_out
+
+        uuid = hwid.get_uuid()
+        mode = d[uuid]['config']['mode']
+        ch_in = d[uuid]['config']['ch_in']
+        ch_out = d[uuid]['config']['ch_out']
+
+        GPIO.setmode(mode)
+        GPIO.getmode()
+        GPIO.setup(ch_in,GPIO.IN)
+        GPIO.setup(ch_out,GPIO.OUT)
+
+       
+except Exception as ex:
+    print(ex)
+    print('###### build config #######')
+    ch_in = []
+    ch_in.append(17)
+    ch_in.append(18)
+    ch_out = []
+    ch_out.append(20)
+    ch_out.append(21)
+    config = {
+        hwid.get_uuid() : {
+            "config" : {
+                "ch_in" : ch_in,
+                "ch_out" : ch_out,
+                "mode" : 11
+            },
+            "setup" : {
+                "17" : 1,
+                "18" : 0,
+                "19" : 0,
+                "20" : 1,
+                "21" : 1
+            }
+        }
+    }
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
