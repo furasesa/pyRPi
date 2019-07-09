1636c8159d88624c640cb29d88d4e5db956fc97b
furasesa <angga.furasesa@gmail.com>
2019-07-09 12:55:20 +0700

    add setup tools

---
+++ server/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyRPi',
+      version='0.1',
+      description='Remote Raspberry with database',
+      url='http://github.com/furasesa/pyRPi',
+      author='Angga Furasesa',
+      license='MIT',
+      packages=['funniest'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
